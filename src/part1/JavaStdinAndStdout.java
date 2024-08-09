9c4ea9f89d814e20f1b9552e6a26ec7eb8fd6241
CristianDaniel24 <cristianponguta789@gmail.com>
2024-08-08 21:42:27 -0500

    part14

---
+++ src/part1/JavaStdinAndStdout.java
@@ -0,0 +1,17 @@
+package part1;
+
+import java.util.Scanner;
+
+public class JavaStdinAndStdout {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int number1 = scanner.nextInt();
+        int number2 = scanner.nextInt();
+        int number3 = scanner.nextInt();
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+
+    }
+}
